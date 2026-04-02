60da4e0f83a665255a47dc43cd731a52180c37db
domanskey <domadam04@gmail.com>
2026-04-02 17:16:39 +0200

    feat: add mobile styling

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex min-h-screen flex-col items-center justify-center">
+        {children}
+      </body>
     </html>
   );
 }
