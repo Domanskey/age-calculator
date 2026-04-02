2458df51fb836e5312ff6aaf926192cc1c89148a
domanskey <domadam04@gmail.com>
2026-04-02 17:25:15 +0200

    feat: add tablet styling

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Form from "next/form";
 export default function Home() {
   return (
     <main className="">
-      <h1 className="mb-4 text-4xl">Age Calculator</h1>
-      <form className="text-md mb-4 flex min-w-2xs flex-col gap-2 text-lg">
+      <h1 className="mb-4 text-4xl sm:text-5xl">Age Calculator</h1>
+      <form className="text-md mb-4 flex min-w-2xs flex-col gap-2 text-lg sm:min-w-96 sm:text-xl">
         <label htmlFor="date">Enter your birth date:</label>
         <input
           className="rounded-md border border-zinc-950 p-1 dark:border-zinc-50"
@@ -19,7 +19,7 @@ export default function Home() {
           Calculate
         </button>
       </form>
-      <p>you're 21 years 10 months old</p>
+      <p className="sm:text-xl">you're 21 years 10 months old</p>
     </main>
   );
 }
